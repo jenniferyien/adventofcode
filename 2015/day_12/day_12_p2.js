11b0e8ede4cd6e3bdedb5d9b0daed6d1e4e2d053
jennifer.yien <jenniferyien@users.noreply.github.com>
2019-09-03 21:25:35 -0400

    solution for day 12 p2 and set up for day 13

--- 2015/day_12/day_12_p2.js
+++ 2015/day_12/day_12_p2.js
@@ -3,9 +3,27 @@ const fs = require('fs')
 const dataInput = fs.readFileSync('./sample_input.txt', 'utf8')
 
 function newSum() {
-  let array = dataInput.match(/(-?[0-9]\d*)/g)
-  // let array = dataInput.replace(/(\r\n|\n|\r)/gm,"\n").trim().split('\n')
+  let json = JSON.parse(dataInput)
+  let sum = returnObjectSum(json, 0)
+  console.log(`Ignoring objects with property "red", the sum of all the numbers is ${sum}.`)
+}
 
+function returnObjectSum(object, sum) {
+  let newS = 0
+  for(let prop in object) {
+    let isArray = object instanceof Array
+    let value = object[prop]
+    let type = typeof value
+    if (type === 'object') {
+      newS += returnObjectSum(value, newS)
+    } else if (type == 'number') {
+      newS += value
+    } else if (!isArray && type == 'string' && value == 'red') {
+      newS = 0
+      break;
+    }
+  }
+  return newS
 }
 
 newSum()
